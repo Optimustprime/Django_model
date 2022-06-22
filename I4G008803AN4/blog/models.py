6ecbb5248d2f875401831c64a32424b4615b9868
TheOptimust <73552453+Optimustprime@users.noreply.github.com>
2022-06-22 13:23:09 +0100

    Update models.py

--- I4G008803AN4/blog/models.py
+++ I4G008803AN4/blog/models.py
@@ -7,5 +7,5 @@ class Post(models.Model):
     title=models.CharField(max_length=200)
     text=models.TextField()
     author=models.ForeignKey(User,on_delete=models.CASCADE)
-    created_Date=models.DateTimeField()
-    published_Date=models.DateTimeField()
+    created_Date=models.DateTimeField(auto_now_add=True)
+    published_Date=models.DateTimeField(auto_now_add=True)
